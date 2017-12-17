a9e779f0c0880bea0f98797c704b607be40dd3ac
Richard Copenhaver <richard.copenhaver@gmail.com>
2017-12-17 17:14:29 -0500

    update windows build script to correct errors

--- jake.cmd
+++ jake.cmd
@@ -1,9 +1,9 @@
 @echo off
 
-if not exist node_modules\.bin\jake[
-    echo Building npm modules:
-    call npm rebuild
-    ]
-
+if not exist node_modules\.bin\jake.cmd (
+  echo Building npm modules:
+  call npm rebuild
+)
 call node_modules\.bin\jake %*
+
 
